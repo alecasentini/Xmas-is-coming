82a59dce554b51227ffd485253515abb095148f5
alecasentini <ale.casentini@gmail.com>
2023-08-14 12:25:30 +0200

    Aggiunto countdown dinamico

--- main.js
+++ main.js
@@ -6,17 +6,29 @@ const secondsElem = document.querySelector('#seconds');
 const endDate = new Date ("December 25 2023 00:00:00 GMT+0200");
 const endDateInMS = endDate.getTime();
 
-const nowInMs = new Date().getTime();
-
-const diff = endDateInMS - nowInMs;
-console.log(endDate)
-
 const secondoInMs = 1000;
 const minuteInMs = 60 * secondoInMs;
 const hourInMs = 60 * minuteInMs;
 const dayInMs = 24 * hourInMs;
 
-daysElem.innerHTML = Math.floor(diff / dayInMs);
-hoursElem.innerHTML = Math.floor ((diff % dayInMs) / hourInMs)
-minutesElem.innerHTML = Math.floor ((diff % hourInMs) / minuteInMs)
-secondsElem.innerHTML = Math.floor ((diff % minuteInMs) / secondoInMs)
+const counterTimer = setInterval(timer, 1000);
+
+function timer(){
+    const nowInMs = new Date().getTime();
+
+    const diff = endDateInMS - nowInMs;
+
+    if (diff > 0){
+        daysElem.innerHTML = Math.floor(diff / dayInMs);
+        hoursElem.innerHTML = Math.floor ((diff % dayInMs) / hourInMs);
+        minutesElem.innerHTML = Math.floor ((diff % hourInMs) / minuteInMs);
+        secondsElem.innerHTML = Math.floor ((diff % minuteInMs) / secondoInMs);
+    }
+
+    else{
+         clearInterval(timer);
+         panelElem.innerHTML = "<h1>Buon Natale!🎅</h1>";
+    }
+    
+    
+}
